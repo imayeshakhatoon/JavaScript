db2f8b7c952a5d577214f8c0fb245b29134f08e6
AYESHA KHATOON <imayeshakhatoon.1@gmail.com>
2026-02-17 01:32:28 +0530

    Updated Object.js with CRUD operations

--- Object.js
+++ Object.js
@@ -7,4 +7,19 @@ const user={
     amount:3400
 }
 
+console.log( typeof user)
+
+console.log(user.age)
+ 
+// CRUD operation
+user.adhar=346667;  //create
+console.log(user )
+
+console.log(user.age)   //read
+
+user.amount=7000;    //update
+console.log(user)
+
+
+delete user.age;  // delete
 console.log(user)
